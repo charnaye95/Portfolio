d75e72da4258e5f9b0be943a4b6c68c68517dffe
Charnaye Grier <charnayegrier@gmail.com>
2022-08-08 18:57:10 -0400

    completed about component

--- my-portfolio/src/Components/About.js
+++ my-portfolio/src/Components/About.js
@@ -2,10 +2,24 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-     <h2 className='title'>
-                About Me
-            </h2>
+    <div className='banner'>
+      <div>
+        <h2 className='title'>
+          About Me
+        </h2>
+      </div>
+      <div className='about-text'>
+        <h3>Ever since I can remember, I had an interest in storytelling, creativity, and the art of communication. I've searched for ways in which I can express my love for it, and discovered I want to channel it to help people. That path led me to web development.  </h3>
+        <br></br>
+        <h3>
+          As a developer, I enjoy problem-solving, the look and feel of an application, and how it’s working. My degree in Communications and time with General Assembly allows me to work efficiently in collaborative settings, and the ability to bring an audience’s perspective to coding. I care about the impact of an application and what it can do for its users.
+        </h3>
+        <br></br>
+        <h3>
+          In my free time, you can find me hanging out with family and friends, binge watching movie and shows, or exploring a new city.
+        </h3>
+      </div>
+
     </div>
   )
 }
--- my-portfolio/src/Components/Intro.js
+++ my-portfolio/src/Components/Intro.js
@@ -1,26 +1,49 @@
 import React from 'react'
 import profile from '../assets/images/profile.jpeg'
+import TypeWriterEffect from 'react-typewriter-effect';
+
 
 function Intro() {
+
     return (
         <div className='banner'>
             <div>
-            <h3 className='hello'>Hello, my name is</h3>
-            <h1 className='name-heading'>
-                Charnaye Grier
-            </h1>
-            <h2 className='title'>
-                Software Developer | Front End Developer
-            </h2>
-            <br></br>
-            <h2 className='title'>
-                I create meaningful applications.
-            </h2>
-            <br></br>
-            <h3 className='brand-statement'>I am a creative, design-minded, and user-driven software developer aiming to create meaningful applications that truly reach people and can add to someone’s life. As a developer, I enjoy problem-solving, the look and feel of an application, and how it’s working. My degree in Communications and time with General Assembly allows me to work efficiently in collaborative settings, and the ability to bring an audience’s perspective to coding. Messaging and access to software for everyone is important to me and if even one application that I create is helpful to someone, I’ve made a difference.</h3>
+                <h3 className='hello'>Hello, my name is</h3>
+                <h1 className='name-heading'>
+                    Charnaye Grier
+                </h1>
+                <h2 className='title'>
+                    <TypeWriterEffect
+                        textStyle={{
+                            fontFamily: 'Jost',
+                            color: '#FFD447',
+                            fontWeight: 700,
+                            fontSize: '30px',
+                        }}
+                        startDelay={2000}
+                        cursorColor="#FFD447"
+                        multiText={[
+                            'Software Developer',
+                            'Front End Developer',
+                            'Software Developer | Front End Developer',
+                        ]}
+                        multiTextDelay={1000}
+                        typeSpeed={40}
+                        hideCursorAfterText={true}
+                    />
+                </h2>
+                <br></br>
+                <h2 className='title'>
+                    I create meaningful applications.
+                </h2>
+                <br></br>
+                <h3 className='brand-statement'>I am a creative, design-minded, and user-driven software developer aiming to create meaningful applications that truly reach people and can add to someone’s life. Messaging and access to software for everyone is important to me and if even one application that I create is helpful to someone, I’ve made a difference.</h3>
             </div>
-            <div>
-                <img src={profile} alt='profile of author' className='profile'></img>
+            <div className='image-container'>
+                <div className='img-card'>
+                    <img src={profile} alt='profile of author' className='profile'></img>
+                </div>
+
             </div>
         </div>
     )
