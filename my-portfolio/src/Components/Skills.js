16d2c851b43ef2734b4a49de6d8af5f10056123c
Charnaye Grier <charnayegrier@gmail.com>
2022-08-09 13:45:06 -0400

    completed skills section

--- my-portfolio/src/Components/Skills.js
+++ my-portfolio/src/Components/Skills.js
@@ -3,14 +3,19 @@ import { motion } from "framer-motion"
 import reactskill from '../assets/images/icons8-react-native-color.svg'
 import jsskill from '../assets/images/icons8-javascript.svg'
 import nodeskill from '../assets/images/icons8-node-js.svg'
+import htmlskill from '../assets/images/icons8-html-5.svg'
 import cssskill from '../assets/images/icons8-css3.svg'
 import bootstrapskill from '../assets/images/icons8-bootstrap.svg'
 import tailwindskill from '../assets/images/icons8-tailwindcss.svg'
+import gitskill from '../assets/images/icons8-git.svg'
 import githubskill from '../assets/images/icons8-github.svg'
 import pythonskill from '../assets/images/icons8-python.svg'
 import djangoskill from '../assets/images/icons8-django.svg'
 import postgresskill from '../assets/images/icons8-postgresql.svg'
 import mongoskill from '../assets/images/icons8-mongodb-a-cross-platform-document-oriented-database-program-75.png'
+import herokuskill from '../assets/images/icons8-heroku.svg'
+import netlifyskill from '../assets/images/icons8-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites.-75.png'
+
 
 function Skills() {
   return (
@@ -55,6 +60,14 @@ function Skills() {
             whileHover={{ scale: 1.5 }}
             className='skill-item'
           >
+            <img src={htmlskill} alt='' className='html-skill'></img>
+            <p className='skill-text'>CSS</p></motion.div>
+          <motion.div
+            // animate={{ scale: 1 }}
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.5 }}
+            className='skill-item'
+          >
             <img src={cssskill} alt='' className='css-skill'></img>
             <p className='skill-text'>CSS</p></motion.div>
           <motion.div
@@ -79,7 +92,15 @@ function Skills() {
             whileHover={{ scale: 1.5 }}
             className='skill-item'
           >
-            <img src={githubskill} alt='' className='git-skill'></img>
+            <img src={gitskill} alt='' className='git-skill'></img>
+            <p className='skill-text'>Git</p></motion.div>
+          <motion.div
+            // animate={{ scale: 1 }}
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.5 }}
+            className='skill-item'
+          >
+            <img src={githubskill} alt='' className='github-skill'></img>
             <p className='skill-text'>Github</p></motion.div>
           <motion.div
             // animate={{ scale: 1 }}
@@ -113,10 +134,22 @@ function Skills() {
           >
             <img src={mongoskill} alt='' className='mongo-skill'></img>
             <p className='skill-text'>MongoDB</p></motion.div>
-
-
-
-
+          <motion.div
+            // animate={{ scale: 1 }}
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.5 }}
+            className='skill-item'
+          >
+            <img src={herokuskill} alt='' className='heroku-skill'></img>
+            <p className='skill-text'>Heroku</p></motion.div>
+          <motion.div
+            // animate={{ scale: 1 }}
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.5 }}
+            className='skill-item'
+          >
+            <img src={netlifyskill} alt='' className='netlify-skill'></img>
+            <p className='skill-text'>Netlify</p></motion.div>
         </motion.div>
 
       </div>
