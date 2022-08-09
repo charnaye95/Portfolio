b9782b2a27b5268462bb91a658087ce3892f7153
Charnaye Grier <charnayegrier@gmail.com>
2022-08-09 14:46:19 -0400

    setting up projects section

--- my-portfolio/src/Components/Projects.js
+++ my-portfolio/src/Components/Projects.js
@@ -2,7 +2,55 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='banner'>
+      <div>
+        <h2 className='title'>
+          Projects
+        </h2>
+      </div>
+      <div className='project-list'>
+        <div className='project-item'>
+          <div className='browser'>
+            <div className='circle1'></div>
+            <div className='circle2'></div>
+            <div className='circle3'></div>
+          </div>
+          <h3 className='project-title'>TherapyReady</h3>
+          <p className='project-description'></p>
+        </div>
+        <br></br>
+        <div className='project-item'>
+          <div className='browser'>
+            <div className='circle1'></div>
+            <div className='circle2'></div>
+            <div className='circle3'></div>
+          </div>
+          <h3 className='project-title'>Align</h3>
+          <p className='project-description'></p>
+        </div>
+        <br></br>
+        <div className='project-item'>
+          <div className='browser'>
+            <div className='circle1'></div>
+            <div className='circle2'></div>
+            <div className='circle3'></div>
+          </div>
+          <h3 className='project-title'>DraftDay</h3>
+          <p className='project-description'></p>
+        </div>
+        <br></br>
+        <div className='project-item'>
+          <div className='browser'>
+            <div className='circle1'></div>
+            <div className='circle2'></div>
+            <div className='circle3'></div>
+          </div>
+          <h3 className='project-title'>Movie Madness</h3>
+          <p className='project-description'></p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
