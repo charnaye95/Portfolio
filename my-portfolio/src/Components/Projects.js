8388a5da91f190a50c3eab92c82e9098a50df65a
Charnaye Grier <charnayegrier@gmail.com>
2022-08-09 17:37:04 -0400

    project section done

--- my-portfolio/src/Components/Projects.js
+++ my-portfolio/src/Components/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import therapyready from '../assets/images/therapyready.netlify.app_.png'
 import align from '../assets/images/peaceful-sunshine-1cdc44.netlify.app_home.png'
+import draftday from '../assets/images/polar-reef-72504.herokuapp.com_.png'
+import moviemadness from '../assets/images/charnaye95.github.io_MovieMadness_.png'
 
 function Projects() {
   return (
@@ -22,8 +24,11 @@ function Projects() {
             <a href='https://therapyready.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={therapyready} alt='therapyready homepage'></img></a>
           </div>
           {/* <p className='project-title'>TherapyReady</p> */}
-          <a href='https://github.com/charnaye95/TherapyReady-Frontend' className='project-github'>Github</a>
-          <p className='project-description'>test descript</p>
+          <a href='https://github.com/charnaye95/TherapyReady-Frontend' target='_blank' rel='noreferrer' className='project-github'>Github</a>
+          <p className='project-description'><b>Django, React, Node.js, CSS, Tailwind CSS </b>
+            <br></br>
+            an application that features clinic and therapist information transparently and health questionnaires as a resource for users to access. Search functionality is included where users can find clinics or therapists by city
+          </p>
         </div>
 
         <br></br>
@@ -39,8 +44,11 @@ function Projects() {
             <a href='https://peaceful-sunshine-1cdc44.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={align} alt='align homepage'></img></a>
           </div>
           {/* <p className='project-title'>TherapyReady</p> */}
-          <a href='https://github.com/aschweik766/breakfast-club-FrontEnd' className='project-github'>Github</a>
-          <p className='project-description'>test descript</p>
+          <a href='https://github.com/aschweik766/breakfast-club-FrontEnd' target='_blank' rel='noreferrer' className='project-github'>Github</a>
+          <p className='project-description'><b>Mongo, Express, React, Node.js, CSS</b>
+            <br></br>
+            an application that displays daily/monthly horoscope information, personal to the user, and includes a dating feature that utilizes swipe functionality
+          </p>
         </div>
 
         <br></br>
@@ -51,13 +59,16 @@ function Projects() {
               <div className='circle1'></div>
               <div className='circle2'></div>
               <div className='circle3'></div>
-              <p className='project-title'>TherapyReady</p>
+              <p className='project-title'>DraftDay</p>
             </div>
-            <a href='https://therapyready.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={therapyready} alt='draftday homepage'></img></a>
+            <a href='https://polar-reef-72504.herokuapp.com/' target='_blank' rel='noreferrer' className='project-link'><img src={draftday} alt='draftday homepage'></img></a>
           </div>
           {/* <p className='project-title'>TherapyReady</p> */}
-          <a href='' className='project-github'>Github</a>
-          <p className='project-description'>test descript</p>
+          <a href='https://github.com/charnaye95/DraftDay' target='_blank' rel='noreferrer' className='project-github'>Github</a>
+          <p className='project-description'>
+            <b>Mongo, Express, Node.js, EJS, CSS</b>
+            <br></br>
+            an application that showcases a draft prospect database of the NBA and ideally scouts could use it throughout a season to keep track of players they would be interested in</p>
         </div>
 
         <br></br>
@@ -67,13 +78,16 @@ function Projects() {
               <div className='circle1'></div>
               <div className='circle2'></div>
               <div className='circle3'></div>
-              <p className='project-title'>TherapyReady</p>
+              <p className='project-title'>Movie Madness</p>
             </div>
-            <a href='https://therapyready.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={therapyready} alt='movie madness homepage'></img></a>
+            <a href='https://charnaye95.github.io/MovieMadness/' target='_blank' rel='noreferrer' className='project-link'><img src={moviemadness} alt='movie madness homepage'></img></a>
           </div>
           {/* <p className='project-title'>TherapyReady</p> */}
-          <a href='' className='project-github'>Github</a>
-          <p className='project-description'>test descript</p>
+          <a href='https://github.com/charnaye95/MovieMadness' target='_blank' rel='noreferrer' className='project-github'>Github</a>
+          <p className='project-description'>
+            <b>HTML, CSS, JavaScript</b>
+            <br></br>
+            a Trivia game using HTML, CSS, and Vanilla JavaScript. The application rendered in the browser, implemented DOM manipulation, integrated semantic markup, and exhibited minimal, DRY code constructed by functions</p>
         </div>
       </div>
 
