15230f0a3675c60fec1269d56b1116a3341ea171
Charnaye Grier <charnayegrier@gmail.com>
2022-08-09 18:11:51 -0400

    worked on contact and footer sections

--- my-portfolio/src/Components/Contact.js
+++ my-portfolio/src/Components/Contact.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <h2 className='contact-title'>Contact</h2>
+      <h3 className='about-text'>Say something here</h3>
+    <a className='email' href='mailto:charnayegrier@gmail.com'>Contact me!</a>
+    </div>
   )
 }
 
