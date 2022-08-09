20f8a85bb741df2db6b6656e709ec2d77c4741b0
Charnaye Grier <charnayegrier@gmail.com>
2022-08-09 16:03:11 -0400

    working on project images and links

--- my-portfolio/src/Components/Projects.js
+++ my-portfolio/src/Components/Projects.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import therapyready from '../assets/images/therapyready.netlify.app_.png'
+import align from '../assets/images/peaceful-sunshine-1cdc44.netlify.app_home.png'
 
 function Projects() {
   return (
@@ -9,44 +11,69 @@ function Projects() {
         </h2>
       </div>
       <div className='project-list'>
-        <div className='project-item'>
-          <div className='browser'>
-            <div className='circle1'></div>
-            <div className='circle2'></div>
-            <div className='circle3'></div>
+        <div className='project1'>
+          <div className='project-item'>
+            <div className='browser'>
+              <div className='circle1'></div>
+              <div className='circle2'></div>
+              <div className='circle3'></div>
+              <p className='project-title'>TherapyReady</p>
+            </div>
+            <a href='https://therapyready.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={therapyready} alt='therapyready homepage'></img></a>
           </div>
-          <h3 className='project-title'>TherapyReady</h3>
-          <p className='project-description'></p>
+          {/* <p className='project-title'>TherapyReady</p> */}
+          <a href='https://github.com/charnaye95/TherapyReady-Frontend' className='project-github'>Github</a>
+          <p className='project-description'>test descript</p>
         </div>
+
         <br></br>
-        <div className='project-item'>
-          <div className='browser'>
-            <div className='circle1'></div>
-            <div className='circle2'></div>
-            <div className='circle3'></div>
+
+        <div className='project2'>
+          <div className='project-item'>
+            <div className='browser'>
+              <div className='circle1'></div>
+              <div className='circle2'></div>
+              <div className='circle3'></div>
+              <p className='project-title'>Align</p>
+            </div>
+            <a href='https://peaceful-sunshine-1cdc44.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={align} alt='align homepage'></img></a>
           </div>
-          <h3 className='project-title'>Align</h3>
-          <p className='project-description'></p>
+          {/* <p className='project-title'>TherapyReady</p> */}
+          <a href='https://github.com/aschweik766/breakfast-club-FrontEnd' className='project-github'>Github</a>
+          <p className='project-description'>test descript</p>
         </div>
+
         <br></br>
-        <div className='project-item'>
-          <div className='browser'>
-            <div className='circle1'></div>
-            <div className='circle2'></div>
-            <div className='circle3'></div>
+
+        <div className='project3'>
+          <div className='project-item'>
+            <div className='browser'>
+              <div className='circle1'></div>
+              <div className='circle2'></div>
+              <div className='circle3'></div>
+              <p className='project-title'>TherapyReady</p>
+            </div>
+            <a href='https://therapyready.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={therapyready} alt='draftday homepage'></img></a>
           </div>
-          <h3 className='project-title'>DraftDay</h3>
-          <p className='project-description'></p>
+          {/* <p className='project-title'>TherapyReady</p> */}
+          <a href='' className='project-github'>Github</a>
+          <p className='project-description'>test descript</p>
         </div>
+
         <br></br>
-        <div className='project-item'>
-          <div className='browser'>
-            <div className='circle1'></div>
-            <div className='circle2'></div>
-            <div className='circle3'></div>
+        <div className='project4'>
+          <div className='project-item'>
+            <div className='browser'>
+              <div className='circle1'></div>
+              <div className='circle2'></div>
+              <div className='circle3'></div>
+              <p className='project-title'>TherapyReady</p>
+            </div>
+            <a href='https://therapyready.netlify.app/' target='_blank' rel='noreferrer' className='project-link'><img src={therapyready} alt='movie madness homepage'></img></a>
           </div>
-          <h3 className='project-title'>Movie Madness</h3>
-          <p className='project-description'></p>
+          {/* <p className='project-title'>TherapyReady</p> */}
+          <a href='' className='project-github'>Github</a>
+          <p className='project-description'>test descript</p>
         </div>
       </div>
 
